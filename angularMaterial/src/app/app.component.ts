18c18fcf21d99f0b4ebe4a03884eabbb1d8886cf
Umesh Kumar Swain <umeshkumarswain5@gmail.com>
2022-09-05 08:45:10 +0530

    mattable implemented

--- angularMaterial/src/app/app.component.ts
+++ angularMaterial/src/app/app.component.ts
@@ -1,30 +1,53 @@
-import { Component } from '@angular/core';
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { filter } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+export class AppComponent implements OnInit {
+
+  displayedColumns: string[] = ['id', 'body', 'title', 'userId'];
+  dataSource :any;
+  pageSizeOptions = [5,10,20,30];
+  totalLength = 0;
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+  @ViewChild(MatSort) sort: MatSort | undefined;
+
+  constructor(private _httpClient: HttpClient) {
+
+  }
+  ngOnInit() {
+    this.loadUsers();
+  }
+  ngAfterViewInit() {
+    if (this.dataSource) {
+    //this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    }
+  }
+  loadUsers() {
+    this._httpClient.get('https://jsonplaceholder.typicode.com/posts').subscribe((data) => {
+      console.log({data})
+      this.dataSource = data;
+      this.dataSource = new MatTableDataSource<any>(this.dataSource);
+      this.totalLength = this.dataSource.filteredData.length;
+      if (this.dataSource) {
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      }
+    })
+  }
+  getSelectedRow(row: any){
+    console.log({row})
+  }
+  applyFilter(value:Event){
+    console.log({event})
+    const element = event?.currentTarget as HTMLInputElement
+    this.dataSource.filter =  element.value.trim().toLocaleLowerCase();
+  }
 }
--- angularMaterial/src/app/app.module.ts
+++ angularMaterial/src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MatTableModule} from '@angular/material/table';
-
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort'
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    
+    BrowserAnimationsModule,
+    HttpClientModule ,
     AppRoutingModule,
-    MatTableModule
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule,
+    MatFormFieldModule
   
   ],
   providers: [],
