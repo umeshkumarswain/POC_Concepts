19a535ac91d73a1329ad349b1b5332ea741421df
Umesh Kumar Swain <umeshkumarswain5@gmail.com>
2022-09-16 14:54:15 +0530

    Clean angular structure

--- angularMaterial/src/app/app.component.ts
+++ angularMaterial/src/app/app.component.ts
@@ -11,43 +11,11 @@ import { filter } from 'rxjs';
 })
 export class AppComponent implements OnInit {
 
-  displayedColumns: string[] = ['id', 'body', 'title', 'userId'];
-  dataSource :any;
-  pageSizeOptions = [5,10,20,30];
-  totalLength = 0;
-  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
-  @ViewChild(MatSort) sort: MatSort | undefined;
-
   constructor(private _httpClient: HttpClient) {
-
   }
+
   ngOnInit() {
-    this.loadUsers();
   }
   ngAfterViewInit() {
-    if (this.dataSource) {
-    //this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-    }
-  }
-  loadUsers() {
-    this._httpClient.get('https://jsonplaceholder.typicode.com/posts').subscribe((data) => {
-      console.log({data})
-      this.dataSource = data;
-      this.dataSource = new MatTableDataSource<any>(this.dataSource);
-      this.totalLength = this.dataSource.filteredData.length;
-      if (this.dataSource) {
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
-      }
-    })
-  }
-  getSelectedRow(row: any){
-    console.log({row})
-  }
-  applyFilter(value:Event){
-    console.log({event})
-    const element = event?.currentTarget as HTMLInputElement
-    this.dataSource.filter =  element.value.trim().toLocaleLowerCase();
   }
 }
